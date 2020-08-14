274f29686495a2ef8daa17b169eda64151da9193
Valmir Santana <valmirsantanape@gmail.com>
2020-08-13 23:34:38 -0300

    import
    librarymath

--- modulos.py
+++ modulos.py
@@ -1,1 +1,5 @@
-num = int(input('Digite um numer: \n'))
+import math
+
+num = int(input('Digite um numero: \n'))
+raiz = math.sqrt(num);
+print("A raiz do numero digitado é {}".format(math.ceil(raiz)))
