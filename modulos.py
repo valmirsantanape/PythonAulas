c993159f8e0518731c7bcd4ddeb9bdd6941f8b62
Valmir Santana <valmirsantanape@gmail.com>
2020-08-14 00:01:56 -0300

    Modulos
    Import

--- modulos.py
+++ modulos.py
@@ -1,5 +1,12 @@
-import math
+from math import sqrt, floor
+import random
+import emoji
 
 num = int(input('Digite um numero: \n'))
-raiz = math.sqrt(num);
-print("A raiz do numero digitado é {}".format(math.ceil(raiz)))
+raiz = sqrt(num);
+print("A raiz do numero {} é {}".format(num, floor(raiz)))
+
+num2 = random.randint(1, 10)
+print('numero aleatorio é {}'.format(num2))
+
+print(emoji.emojize( 'Ola mundo :earth_americas:', use_aliases=True))
