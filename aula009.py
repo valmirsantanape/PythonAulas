2defdf8a39a3077aa9c83d656a81dfdc38cfc892
Valmir Santana <valmirsantanape@gmail.com>
2020-08-15 20:47:40 -0300

    aula009

---
+++ aula009.py
@@ -0,0 +1,8 @@
+frase = 'Curso em Video Python'
+print(frase[0:14])
+print(frase.strip())
+print(len(frase))
+print(frase[0])
+print(frase.replace('Python', 'Android'))
+print(frase)
+print(frase.find())
