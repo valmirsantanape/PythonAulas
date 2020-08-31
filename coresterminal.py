46cef652ec5fd83026424a4f3cf1d5a7aa9adfd7
Valmir Santana <valmirsantanape@gmail.com>
2020-08-31 18:53:04 -0300

    coresnoterminal.py

---
+++ coresterminal.py
@@ -0,0 +1,8 @@
+print('\033[1;31;43m Ola, mundo\033[m')
+print('\033[4;33;44m Ola, mundo\033[m')
+print('\033[1;35;43m Ola, mundo\033[m')
+print('\033[30;42m Ola, mundo\033[m')
+print('\033[0;33;44m Ola, mundo\033[m')
+print('\033[7;30m Ola, mundo\033[m')
+
+print(2+2==4)
