898a92b503617875e1ea2ad81383d3c88deda00e
Valmir Santana <valmirsantanape@gmail.com>
2020-09-16 22:01:09 -0300

    aula013.py
    estrutura de repetição for

---
+++ aula013-for.py
@@ -0,0 +1,6 @@
+i = int(input('Digite o inicio da contagem: '))
+f = int(input('Digite o final da contagem: '))
+p = int(input('Ao passo de: '))
+for c in range(i, f+1 ,p):
+    print(c)
+print('\nFIM')
