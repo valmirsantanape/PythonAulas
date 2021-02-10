1450b3ebad686f59656de8154dafc053a9703dbb
valmirsantanape <valmirsantanape@gmail.com>
2021-02-09 22:37:12 -0300

    update
    segunda parte

--- aula019-dicionario.py
+++ aula019-dicionario.py
@@ -1,3 +1,4 @@
+"""
 pessoa = {'nome': 'valmir', 'sexo': 'M', 'idade': 29}
 print(f'O {pessoa["nome"]} é do sexo {pessoa["sexo"]} e tem {pessoa["idade"]} anos')
 print(pessoa.keys())
@@ -11,3 +12,17 @@ print()
 pessoa['peso'] = 98.5
 for k, v in pessoa.items():
     print(f'{k} = {v}')
+"""
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2= {'uf': 'São Paulo', 'sigla': 'SP'}
+estado3 = {'uf': 'Pernambuco', 'sigla': 'PE'}
+estado4 = {'uf': 'Alagoas', 'sigla': 'AL'}
+estado5 = {'uf': 'Santa Catarina', 'sigla': 'SC'}
+estado6 = {'uf': 'Rio Grande do Sul','sigla': 'RS'}
+
+brasil = list()
+brasil.append(estado1)
+brasil.append(estado2)
+
+print(brasil[0])
+print(brasil[1])
