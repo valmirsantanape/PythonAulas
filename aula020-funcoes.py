7d4b1695878cbdd1cebc56a52ae356bbae4a2b88
Valmir Santana <valmirsantanape@gmail.com>
2022-03-30 00:18:17 -0300

    update

---
+++ aula020-funcoes.py
@@ -0,0 +1,6 @@
+def titulo(msg):
+    print('-' * 30)
+    print(msg)
+    print('-' * 30)
+
+titulo('      PYTHON É MUITO BOM')
---
+++ aula020a.py
@@ -0,0 +1,7 @@
+#programa principal
+def soma(a, b):
+    s = a + b
+    print(s)
+soma(5, 6)
+soma(8, 9)
+soma(3, 5)
